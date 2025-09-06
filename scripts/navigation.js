2e0598156e272dde510cd5ce58a818a3f1e4d28b
Max-commits <tobamicar@gmail.com>
2025-09-06 16:34:42 +0100

    B

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -8,3 +8,10 @@ const nav = document.getElementById("navigation");
 menuButton.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
+const menuButton = document.getElementById("menu");
+const nav = document.getElementById("navigation");
+
+menuButton.addEventListener("click", () => {
+  const isOpen = nav.classList.toggle("open");
+  menuButton.setAttribute("aria-expanded", isOpen);
+});
