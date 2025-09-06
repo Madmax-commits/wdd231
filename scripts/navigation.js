7246b0fef746612107e8c6053c666161ce86ec64
Max-commits <tobamicar@gmail.com>
2025-09-06 16:44:43 +0100

    N

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,6 @@
-document.getElementById("menu").addEventListener("click", () => {
-  document.getElementById("navigation").classList.toggle("open");
-});
-
 const menuButton = document.getElementById("menu");
 const nav = document.getElementById("navigation");
 
 menuButton.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
-const menuButton = document.getElementById("menu");
-const nav = document.getElementById("navigation");
-
